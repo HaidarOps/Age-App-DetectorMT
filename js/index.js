a08b54d644fe6e9ccaf169de43d7ec247b8a95d1
Haidar Kassem <haidarskassem@outlook.com>
2025-09-16 15:32:37 +0100

    Image selection fixed and model responds with age

--- js/index.js
+++ js/index.js
@@ -1,9 +1,41 @@
 // When button is pressed, take the image and push it through a model
 
 
+
+import Human from './../dist/human.esm.js'; // for direct import must use path to module, not package name
+
+
+// Used to analyze image on the with Human model api
+async function analyzeImageHumanModel(){
+
+
+    let img = document.getElementById('display-image');
+    if (img == null){
+        // Then create the img
+        console.log("There is no image...");
+    }
+   // Sample configuration from docs
+  const myConfig = {
+  face: {
+    enabled: true,
+    detector: { rotation: true, return: true },
+    mesh: { enabled: true },
+    description: { enabled: true },
+  },
+};
+  const human = new Human(myConfig);  
+  const firstImage = await human.detect(img.src);
+  console.log(firstImage)
+
+  return 0;
+}
+
 function createImage(image_path) {
 
-    
+   
+
+
+
     let img = document.getElementById('display-image');
     if (img == null){
         // Then create the img
@@ -30,7 +62,7 @@ function createImage(image_path) {
 function ImagePaser(){
 
     console.log(document.getElementById('input_image').value);
-    let current_path = document.getElementById('input_image')
+    let current_path = document.getElementById('input_image');
     createImage(current_path);
 
    return 0;
